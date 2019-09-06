862d9b8d749a02bea5986c7808c637405b39d9a1
Louis Shinohara <louishinohara@gmail.com>
2019-09-06 16:33:57 -0400

    update

--- My Portfolio Website/js/custom.js
+++ My Portfolio Website/js/custom.js
@@ -4,7 +4,7 @@ $(document).ready(function() { //when document(DOM) loads completely.
   // Transition effect for navbar 
   $(window).scroll(function() { //function is called while you scrolls 
     // checks if window is scrolled more than 300px, adds/removes solid class
-    if($(this).scrollTop() > 300) { 
+    if($(this).scrollTop() > 100) { 
         $('.navbar').addClass('solid'); //add class 'solid' in any element which has class 'navbar'
     } else {
         $('.navbar').removeClass('solid'); //remove class 'solid' in any element which has class 'navbar'
